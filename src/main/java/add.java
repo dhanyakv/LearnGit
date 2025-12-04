6136451a908df2d3656ca3599a926d15d1a29dc7
kvdhanya <kvdhanya@gmail.com>
2025-12-04 14:00:50 -0500

    add file changed a bit with the output

--- src/main/java/add.java
+++ src/main/java/add.java
@@ -1,8 +1,10 @@
 public class add {
     public static void main(String[] args) {
+        System.out.println("Hello World");
         int a = 10;
         int b = 20;
         int c = a + b;
         System.out.println("The sum is: " + c);
+        System.out.println("The difference is: " + c);
     }
 }
